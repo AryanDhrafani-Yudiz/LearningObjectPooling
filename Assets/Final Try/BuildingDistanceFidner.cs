d15ca92d335519569d4a8cb0181c68646fb4714d
AryanDhrafani-Yudiz <aryan.dhrafani@yudiz.com>
2024-03-07 19:04:59 +0530

    Updated BuildingDistance

--- Assets/Final Try/BuildingDistanceFidner.cs
+++ Assets/Final Try/BuildingDistanceFidner.cs
@@ -2,11 +2,23 @@ using UnityEngine;
 
 public class BuildingDistanceFidner : MonoBehaviour
 {
-    void Update()
+    private Vector2 screenBounds;
+
+    private void FixedUpdate()
     {
-        if(Input.GetKeyDown(KeyCode.Tab))
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        Debug.Log(screenBounds);
+        if (this.gameObject.transform.position.x > screenBounds.x)
         {
-            this.gameObject.SetActive(false);
+            //this.gameObject.SetActive(false);
+            Debug.Log(screenBounds);
         }
+    }
+        void Update()
+    {            
+        //if (Camera.main.transform.position.x + this.transform.position.x > 0)
+        //{
+        //    this.gameObject.SetActive(false);
+        //}
     }
 }
